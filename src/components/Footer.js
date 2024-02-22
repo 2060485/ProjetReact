31d8242f778d901d2ec2875cebd65a2d6b94bac6
2289642 <2289642@collegemv.qc.ca>
2024-02-22 11:41:02 -0500

    footer copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
                 <nav class="fixed-bottom mt-5 navbar navbar-dark border bg-dark">
                     <div class="mx-2 container-fluid border border">
                     </div>
-                    <small class="p-2 display-3 navbar-brand text-center mx-auto">© 2024 Grimace Gastronomy, Inc</small>
+                    <small class="p-2 display-3 navbar-brand text-center mx-auto">© 2024 Grimace Gastronomy, Inc. Tous droits réservés.</small>
                 </nav>
             </div>
         </div>

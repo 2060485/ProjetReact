09ff886b978efe63caae57cd5b5601308d63d682
Mathieu Gouveia Sousa <2233635@collegemv.qc.ca>
2024-04-02 17:58:21 -0400

    notfound

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,10 +1,18 @@
-
+import "./notfound.css";
 
 function NotFound() {
     return (
-        <div>
-            NotFound
+        <>
+        <div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center">
+                <strong class="display-1">404</strong>
+                <p class="my-3 lead">
+                    Perdu?
+                  </p>
+                <a href="/" class="btn btn-primary">Retour à GG</a>
+            </div>
         </div>
+        </>
     );
 }
 
